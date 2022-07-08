d54a49ec160e2f5e54eed569126a4a5ae2c820b5
aoiiiiii0211 <103923470+aoiiiiii0211@users.noreply.github.com>
2022-07-08 15:53:19 +0900

    Add files via upload

---
+++ aaa-aa-aaa-aa/template-parts/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying footer.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<footer id="site-footer" class="bg-dark text-light w-100" role="contentinfo">
+	<h6>&copy; test@aaaaoi</h6>
+</footer>
